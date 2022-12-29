52178a569ad8457f034e1789244cc12e8d0c9722
Ziad Tariq <121635243+ziadtariq1@users.noreply.github.com>
2022-12-29 22:13:44 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,40 +1,36 @@
-﻿using System;
-
-namespace PerfectNumbers
+﻿class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // Read n1 and n2 from the user
-            Console.WriteLine("Enter n1: ");
-            int n1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter n2: ");
-            int n2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter first number: ");
+        int n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        int n2 = int.Parse(Console.ReadLine());
 
-            // Find and print the perfect numbers from n1 to n2
-            Console.WriteLine("Perfect numbers from " + n1 + " to " + n2 + ": ");
-            for (int i = n1; i <= n2; i++)
+
+        Console.WriteLine("Prime numbers from " + n1 + " to " + n2 + ": ");
+        for (int i = n1; i <= n2; i++)
+        {
+            if (IsPrimeNumber(i))
             {
-                if (IsPerfectNumber(i))
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine(i);
             }
         }
+    }
 
-        // Returns true if the given number is a perfect number, false otherwise
-        static bool IsPerfectNumber(int n)
+
+    static bool IsPrimeNumber(int n)
+    {
+        if (n == 1 || n == 0)
         {
-            int sum = 0;
-            for (int i = 1; i < n; i++)
-            {
-                if (n % i == 0)
-                {
-                    sum += i;
-                }
-            }
-            return sum == n;
+            return false;
         }
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+
+                return false;
+        }
+        return true;
     }
 }
